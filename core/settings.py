9552f98d03f2816794e442a583dd149a821a5d0c
thetanomial <iyamthor10@gmail.com>
2025-03-20 12:25:10 +0530

    make change to views.py response in api app

--- core/settings.py
+++ core/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = 'django-insecure-hu*5(v531mj%r3i!4b#(eebo@85y8t)eki(e%qeydz=&r2cc65
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost',f'${os.getenv("DROPLET_IP")}']
+ALLOWED_HOSTS = ['localhost', os.getenv("DROPLET_IP", "127.0.0.1")]
+
 
 
 # Application definition
